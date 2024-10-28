3b79545863bdf00f3c1be17d4c7105447c5fce4b
Prakash Luitel <prakashluitel991@gmail.com>
2024-10-28 15:53:24 +0545

    Added the url for the items

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,10 +9,15 @@ urlpatterns = [
     path("filemanager/", FileManagerView.as_view(), name="filemanager"),
     path("basic-settings/", SettingsView.as_view(), name="settings"),
     path("informative-settings/", AboutSettingsView.as_view(), name="aboutus"),
+
     path("page/add", PageView.as_view(), name="addpage"),
     path("page/list", PageListView.as_view(), name="listpage"),
     path("page/edit", PageEditView.as_view(), name="editpage"),
     
+    path("add", Items.as_view(), name="add"),
+    path("list", ItemsListView.as_view(), name="list"),
+    path("edit", ItemsEditView.as_view(), name="edit"),
+
     path("delete/", DeleteView.as_view(), name="delete"),
     path("delete/final/", DeleteFinalView.as_view(), name="deletefinal"),
 
