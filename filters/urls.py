8bc3e1a3c73f98e20fbe85adda460401edea7d07
Prakash Luitel <prakashluitel991@gmail.com>
2024-11-08 14:26:20 +0545

    new app 'webiste' added

--- filters/urls.py
+++ filters/urls.py
@@ -9,13 +9,11 @@ urlpatterns = [
     path("page/add", FilterPageView.as_view(), name="addpage"),
     path("page/list", FilterPageListView.as_view(), name="listpage"),
     path("page/edit", FilterPageEditView.as_view(), name="editpage"),
-    
     path("add", FilterItems.as_view(), name="add"),
     path("list", FilterItemsListView.as_view(), name="list"),
     path("edit", FilterItemsEditView.as_view(), name="edit"),
 
     path("delete/", FilterDeleteView.as_view(), name="delete"),
     path("delete/final/", FilterDeleteFinalView.as_view(), name="deletefinal"),
-
 
 ]
